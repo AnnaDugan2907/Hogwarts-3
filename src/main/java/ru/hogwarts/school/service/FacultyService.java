1e556ec665e9d23b8c0e5c83b1523a5c1213496b
AnnaDugan2907 <annadugan32@gmail.com>
2026-01-12 02:48:41 +0500

    Added homework

--- src/main/java/ru/hogwarts/school/service/FacultyService.java
+++ src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.repository.FacultyRepository;
@@ -10,6 +12,7 @@ import java.util.Optional;
 @Service
 public class FacultyService {
 
+    private static final Logger logger = LoggerFactory.getLogger(FacultyService.class);
     private final FacultyRepository facultyRepository;
 
     public FacultyService(FacultyRepository facultyRepository) {
@@ -17,31 +20,37 @@ public class FacultyService {
     }
 
     public Faculty createFaculty(Faculty faculty) {
+        logger.info("Was invoked method for create faculty");
         return facultyRepository.save(faculty);
     }
 
     public Faculty findFaculty(long id) {
-        Optional<Faculty> optionalFaculty = facultyRepository.findById(id);
-        return optionalFaculty.orElse(null); // Возвращает null, если не найден
+        logger.info("Was invoked method for find faculty");
+        return facultyRepository.findById(id).get();
     }
 
     public Faculty editFaculty(Faculty faculty) {
+        logger.info("Was invoked method for edit faculty");
         return facultyRepository.save(faculty);
     }
 
     public void deleteFaculty(long id) {
+        logger.info("Was invoked method for delete faculty");
         facultyRepository.deleteById(id);
     }
 
     public Collection<Faculty> getAllFaculty() {
+        logger.info("Was invoked method for get all faculty");
         return facultyRepository.findAll();
     }
 
     public Collection<Faculty> findByNameIgnoreCase(String name) {
+        logger.info("Was invoked method for find by name ignore case");
         return facultyRepository.findByNameIgnoreCase(name);
     }
 
     public Collection<Faculty> findByColorIgnoreCase(String color) {
+        logger.info("Was invoked method for find by color ignore case");
         return facultyRepository.findByColorIgnoreCase(color);
     }
 }
