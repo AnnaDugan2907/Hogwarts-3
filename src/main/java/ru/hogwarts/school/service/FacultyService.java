f05514be58ab4a73a4e35a20d2c08ec17a90a625
AnnaDugan2907 <annadugan32@gmail.com>
2026-01-19 01:43:29 +0500

    Added homework

--- src/main/java/ru/hogwarts/school/service/FacultyService.java
+++ src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -3,13 +3,11 @@ package ru.hogwarts.school.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
 import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.repository.FacultyRepository;
 
 import java.util.Collection;
 import java.util.Comparator;
-import java.util.Optional;
 import java.util.stream.IntStream;
 
 @Service
